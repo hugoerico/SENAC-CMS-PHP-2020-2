fcb795159157898c30c7f75233836cd8167c84f2
SAsuke019 <hugo.019@hotmail.com>
2020-11-19 20:05:22 -0300

    aula do dia 17-11

---
+++ crud_cms/lista_tpl.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud cms</title>
+</head>
+<body>
+    <h1>Crud cms</h1>
+    <br><br>
+
+<form method="post">
+
+    <table border="1" width="50%">
+    <tr>
+        <td>Nome</td>
+        <td>Whatsapp</td>
+        <td></td>
+
+        <?php
+        
+            foreach($contatos as $key => $value){
+
+                echo "<tr>
+                        <td>{$value->nome}</td>
+                        <td>{$value->whatsapp}</td>
+                        <td><a href='?page={$_GET['page']}&apagar={value->id}'>Apagar</a></td>
+                        </tr>";
+            }
+        
+        ?>
+        <tr>
+            <td><input type="text" name="nome"></td>
+            <td><input type="text" name="whatsapp"></td>
+            <td><?php submit_button('Gravar'); ?></td>
+            
+        </tr>
+    </tr>
+    </table>
+
+    </form>
+
+</body>
+</html>
